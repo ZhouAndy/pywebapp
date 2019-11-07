276daa15d4faed1d3510f8078059ab4e3e489dbc
Andy Zhou <32149126+ZhouAndy@users.noreply.github.com>
2019-11-07 14:01:30 -0800

    Update startcontainer.sh
    changed back to n.virginia

--- appspec/startcontainer.sh
+++ appspec/startcontainer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 $(aws ecr get-login --no-include-email --region us-west-2)
-docker pull 376180697510.dkr.ecr.us-west-2.amazonaws.com/pywebapp:latest || {
+docker pull 376180697510.dkr.ecr.us-east-1.amazonaws.com/zhouandy/pywebapp:latest || {
     echo "ERROR: docker pull failed. Sleeping for 10 minutes to allow investigation..."
     sleep 600
     exit 1
